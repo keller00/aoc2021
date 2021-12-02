95d228103e7be18b1979bac34b3b4b82696b20ca
Mark Keller <8452750+keller00@users.noreply.github.com>
2021-12-01 21:52:35 -0800

    done with day 02

---
+++ day02/part1.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import argparse
+import pathlib
+
+import pytest
+
+this_dir = pathlib.Path(__file__).parent.resolve()
+
+
+def solve(_input: str) -> tuple[int, int]:
+    input_numbers = [e.split(" ") for e in _input.splitlines()]
+
+    depth = 0
+    position = 0
+    for command, m in input_numbers:
+        n = int(m)
+        if command == "forward":
+            position += n
+        elif command == "up":
+            depth -= n
+        elif command == "down":
+            depth += n
+        else:
+            raise Exception(f"what is {command}")
+
+    return position, depth
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?',
+                        default=str(this_dir / "input.txt"))
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        position, depth = solve(f.read())
+        print(position * depth)
+
+    return 0
+
+
+@pytest.mark.parametrize(("input_file", "expected_result"), [
+    (this_dir / "sample_input.txt", (15, 10)),
+])
+def test_sample_data(
+        input_file: pathlib.Path,
+        expected_result: tuple[int, int],
+) -> None:
+    assert solve(input_file.read_text()) == expected_result
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
