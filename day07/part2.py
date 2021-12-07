0ddc1eb226edd5b47e73ba848e0fe04ef575ec35
Mark Keller <8452750+keller00@users.noreply.github.com>
2021-12-06 21:41:03 -0800

    finished day 7

---
+++ day07/part2.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import argparse
+import pathlib
+
+import pytest
+
+this_dir = pathlib.Path(__file__).parent.resolve()
+
+
+def solve(_input: str) -> int:
+    input_lines = _input.splitlines()
+    crabs = [int(e) for e in input_lines[0].split(",")]
+    solutions = {}
+
+    for n in range(min(crabs), max(crabs)+1):
+        solutions[n] = 0
+        for crab in crabs:
+            difference = abs(crab - n)
+            solutions[n] += difference * (difference + 1) // 2
+    return min(solutions.values())
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?',
+                        default=str(this_dir / "input.txt"))
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(solve(f.read()))
+
+    return 0
+
+
+@pytest.mark.parametrize(("input_file", "expected_result"), [
+    (this_dir / "sample_input.txt", 168),
+    (this_dir / "input.txt", 93699985),
+])
+def test_sample_data(input_file: pathlib.Path, expected_result: int) -> None:
+    assert solve(input_file.read_text()) == expected_result
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
